053c95754b279b58d0ff06ceded76ef63435afe6
Hameh Jamshidi <Jamshidi@moka.rrar>
2022-08-03 17:06:56 +0430

    finish navbar

---
+++ src/components/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+  {
+    title: 'ثبت عملیات جدید',
+    path: '/marketing',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'ثبت ایستگاه جدید',
+    path: '/consulting',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'تعریف مدیران و روسا',
+    path: '/design',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'تعریف کاربر یا اپراتور',
+    path: '/development',
+    cName: 'dropdown-link',
+  },
+  {
+    title: 'ثبت نام ماده جدید',
+    path: '/development',
+    cName: 'dropdown-link',
+  },
+  {
+    title: 'ثبت مشخصه کنترلی جدید',
+    path: '/development',
+    cName: 'dropdown-link',
+  }
+];
